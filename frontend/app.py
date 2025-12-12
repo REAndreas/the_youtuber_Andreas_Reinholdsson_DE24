1657a0f2eeb48162bcec04fbaadb55e4509af01b
Andreas <andreas.reinhol@gmail.com>
2025-12-12 09:36:56 +0100

    added a picture

--- frontend/app.py
+++ frontend/app.py
@@ -2,15 +2,23 @@ import streamlit as st
 import requests
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 
 load_dotenv()
 
+ASSETS_PATH = Path(__file__).parents[1]
 url = f"https://youtuber-andreas-de24.azurewebsites.net/rag/query?code={os.getenv('API_KEY')}"
 
 
 def layout():
     st.markdown("# The Youtuber")
-    st.markdown("Ask about data engineering topics")
+
+    left_column, right_column = st.columns(2)
+
+    left_column.markdown("Ask about data engineering topics")
+
+    right_column.image(ASSETS_PATH / f"kokchun.png")
+    
     text_input = st.text_input(label= "Ask a question")
 
     if st.button("Inquire") and text_input != "":
